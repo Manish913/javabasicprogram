47a77512d38e7a4458dd17241d74a45399d5c2dc
manish <manishmahik33@gmail.com>
2021-05-21 17:06:18 +0530

    my fisrt commit

---
+++ src/basic/Add.java
@@ -0,0 +1,13 @@
+package basic;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a =39;
+		int b=32;
+		System.out.println("sum ="+(a+b));
+	}
+
+}
---
+++ src/basic/HelloWord.java
@@ -0,0 +1,10 @@
+package basic;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hay manish");
+	}
+
+}
